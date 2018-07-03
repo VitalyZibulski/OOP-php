8884a20a8955e941b58c5dffb9e3e7d53ee2538e
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-07-03 12:12:55 +0300

    add lesson interfaces

---
+++ lesson-4-interfaces/index.php
@@ -0,0 +1,34 @@
+<?php
+
+	class Task
+	{
+
+
+
+
+	}
+
+	interface TaskStorageInterface
+	{
+		public function get($id);
+		public function store(Task $task);
+	}
+
+	class MySqlDatabaseTaskStorage implements TaskStorageInterface
+	{
+		public function get($id)
+		{
+
+
+
+		}
+
+		public function store(Task $task)
+		{
+
+
+
+		}
+
+	}
+
